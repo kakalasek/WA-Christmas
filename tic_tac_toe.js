1cb2d0c9145b990f7d548db9988dc16cdd690e2e
kakalasek <honzicekkj@seznam.cz>
2023-12-28 18:18:32 +0100

    total rework

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -4,7 +4,7 @@ let turn = true; // true = circle, false = cross
 
 /* Generates the playing field */
 const generateField = () => {
-  for (i = 0; i < 100; i++) {
+  for (i = 0; i < 9; i++) {
     playingField.appendChild(generateNode(i));
   }
 };
@@ -12,9 +12,12 @@ const generateField = () => {
 /* Generates one node of the playing field */
 const generateNode = (index) => {
   const node = document.createElement("div");
-  node.setAttribute("id", index);
+  node.setAttribute("id", "f" + index);
   node.addEventListener("click", () => {
-    if (node.children.length == 0) node.appendChild(generateCrossOrCircle());
+    if (node.children.length == 0) {
+      node.appendChild(generateCrossOrCircle());
+      checkForWin(node);
+    }
   });
   return node;
 };
@@ -32,6 +35,43 @@ const generateCrossOrCircle = () => {
     return element;
   }
 };
+
+/* Check if a player has won */
+/* NOTE: This method is probably going to be super ugly, I am sorry for that but I am too lazy to think about it longer, so Im just going to brute force and check every possibility.
+         Also the way I have implemented my field is super unpractical and does not really allow for any better approach, or as far as Im concerned. 
+         Please dont try to understand what exactly happens inside this method. I barely know it myself...*/
+// const checkForWin = (element) => {
+//   let elementIndex = element.id.split("f")[1];
+//   let adjentElements = [
+//     playingField.querySelector(`#f${elementIndex - 1}`),
+//     playingField.querySelector(`#f${elementIndex - 11}`),
+//     playingField.querySelector(`#f${elementIndex - 10}`),
+//     playingField.querySelector(`#f${elementIndex - 9}`),
+//     playingField.querySelector(`#f${Number(elementIndex) + 1}`),
+//     playingField.querySelector(`#f${Number(elementIndex) + 11}`),
+//     playingField.querySelector(`#f${Number(elementIndex) + 10}`),
+//     playingField.querySelector(`#f${Number(elementIndex) + 9}`),
+//   ];
+//   let crossOrCircle = element.querySelector(".circle") ? ".circle" : ".cross";
+
+//   console.log(adjentElements[0].querySelector(crossOrCircle) !== null);
+//   console.log(adjentElements[4].querySelector(crossOrCircle) !== null);
+
+//   if (
+//     (adjentElements[0].querySelector(crossOrCircle) !== null &&
+//       adjentElements[4].querySelector(crossOrCircle) !== null) ||
+//     (adjentElements[2].querySelector(crossOrCircle) !== null &&
+//       adjentElements[6].querySelector(crossOrCircle) !== null) ||
+//     (adjentElements[7].querySelector(crossOrCircle) !== null &&
+//       adjentElements[3].querySelector(crossOrCircle) !== null) ||
+//     (adjentElements[1].querySelector(crossOrCircle) !== null &&
+//       adjentElements[5].querySelector(crossOrCircle) !== null) ||
+//     (adjentElements[1].querySelector(crossOrCircle) !== null &&
+//       adjentElements[5].querySelector(crossOrCircle) !== null)
+//   ) {
+//     alert("WIN");
+//   }
+// };
 
 /* Executed when page is loaded */
 generateField();
