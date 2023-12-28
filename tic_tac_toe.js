591e1d8a86f9d8dc05d009f3825ec3975b37c8d2
kakalasek <honzicekkj@seznam.cz>
2023-12-28 16:35:17 +0100

    circles and crosses, comments

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,5 +1,6 @@
 /* Variable declarations */
 const playingField = document.querySelector("#playing-field");
+let turn = true; // true = circle, false = cross
 
 /* Generates the playing field */
 const generateField = () => {
@@ -12,7 +13,25 @@ const generateField = () => {
 const generateNode = (index) => {
   const node = document.createElement("div");
   node.setAttribute("id", index);
+  node.addEventListener("click", () => {
+    if (node.children.length == 0) node.appendChild(generateCrossOrCircle());
+  });
   return node;
 };
 
+/* Generates either cross or circle */
+const generateCrossOrCircle = () => {
+  const element = document.createElement("div");
+  if (turn) {
+    element.classList.add("circle");
+    turn = !turn;
+    return element;
+  } else {
+    element.classList.add("cross");
+    turn = !turn;
+    return element;
+  }
+};
+
+/* Executed when page is loaded */
 generateField();
