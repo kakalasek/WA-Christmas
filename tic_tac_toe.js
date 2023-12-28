a8b4b795ccba94cd0eef850a44e5b80b21210e2e
kakalasek <honzicekkj@seznam.cz>
2023-12-28 21:35:30 +0100

    done

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,6 +1,7 @@
 /* Variable declarations */
 const playingField = document.querySelector("#playing-field");
 let turn = true; // true = circle, false = cross
+let win = false;
 
 /* Generates the playing field */
 const generateField = () => {
@@ -14,10 +15,11 @@ const generateNode = (index) => {
   const node = document.createElement("div");
   node.setAttribute("id", "f" + index);
   node.addEventListener("click", () => {
-    if (node.children.length == 0) {
+    if (node.children.length == 0 && !win) {
       node.appendChild(generateCrossOrCircle());
-      checkForWin(node);
+      checkForWin();
     }
+    win ? alert("WIN!!\nRefresh The Page To Restart") : console.log();
   });
   return node;
 };
@@ -37,41 +39,50 @@ const generateCrossOrCircle = () => {
 };
 
 /* Check if a player has won */
-/* NOTE: This method is probably going to be super ugly, I am sorry for that but I am too lazy to think about it longer, so Im just going to brute force and check every possibility.
-         Also the way I have implemented my field is super unpractical and does not really allow for any better approach, or as far as Im concerned. 
-         Please dont try to understand what exactly happens inside this method. I barely know it myself...*/
-// const checkForWin = (element) => {
-//   let elementIndex = element.id.split("f")[1];
-//   let adjentElements = [
-//     playingField.querySelector(`#f${elementIndex - 1}`),
-//     playingField.querySelector(`#f${elementIndex - 11}`),
-//     playingField.querySelector(`#f${elementIndex - 10}`),
-//     playingField.querySelector(`#f${elementIndex - 9}`),
-//     playingField.querySelector(`#f${Number(elementIndex) + 1}`),
-//     playingField.querySelector(`#f${Number(elementIndex) + 11}`),
-//     playingField.querySelector(`#f${Number(elementIndex) + 10}`),
-//     playingField.querySelector(`#f${Number(elementIndex) + 9}`),
-//   ];
-//   let crossOrCircle = element.querySelector(".circle") ? ".circle" : ".cross";
+/* NOTE: I know this brute force approach is awful but Im too lazy to think of something more convenient. Plus the way my field works, which is really impractical, does not really allow for anything else, or at least as far as Im concerned */
+const checkForWin = () => {
+  let crossOrCircle = turn ? ".cross" : ".circle";
+  let fieldElements = [
+    playingField.querySelector("#f0"),
+    playingField.querySelector("#f1"),
+    playingField.querySelector("#f2"),
+    playingField.querySelector("#f3"),
+    playingField.querySelector("#f4"),
+    playingField.querySelector("#f5"),
+    playingField.querySelector("#f6"),
+    playingField.querySelector("#f7"),
+    playingField.querySelector("#f8"),
+  ];
 
-//   console.log(adjentElements[0].querySelector(crossOrCircle) !== null);
-//   console.log(adjentElements[4].querySelector(crossOrCircle) !== null);
-
-//   if (
-//     (adjentElements[0].querySelector(crossOrCircle) !== null &&
-//       adjentElements[4].querySelector(crossOrCircle) !== null) ||
-//     (adjentElements[2].querySelector(crossOrCircle) !== null &&
-//       adjentElements[6].querySelector(crossOrCircle) !== null) ||
-//     (adjentElements[7].querySelector(crossOrCircle) !== null &&
-//       adjentElements[3].querySelector(crossOrCircle) !== null) ||
-//     (adjentElements[1].querySelector(crossOrCircle) !== null &&
-//       adjentElements[5].querySelector(crossOrCircle) !== null) ||
-//     (adjentElements[1].querySelector(crossOrCircle) !== null &&
-//       adjentElements[5].querySelector(crossOrCircle) !== null)
-//   ) {
-//     alert("WIN");
-//   }
-// };
+  if (
+    (fieldElements[0].querySelector(crossOrCircle) !== null &&
+      fieldElements[3].querySelector(crossOrCircle) !== null &&
+      fieldElements[6].querySelector(crossOrCircle)) ||
+    (fieldElements[1].querySelector(crossOrCircle) !== null &&
+      fieldElements[4].querySelector(crossOrCircle) !== null &&
+      fieldElements[7].querySelector(crossOrCircle)) ||
+    (fieldElements[2].querySelector(crossOrCircle) !== null &&
+      fieldElements[5].querySelector(crossOrCircle) !== null &&
+      fieldElements[8].querySelector(crossOrCircle)) ||
+    (fieldElements[0].querySelector(crossOrCircle) !== null &&
+      fieldElements[1].querySelector(crossOrCircle) !== null &&
+      fieldElements[2].querySelector(crossOrCircle)) ||
+    (fieldElements[3].querySelector(crossOrCircle) !== null &&
+      fieldElements[4].querySelector(crossOrCircle) !== null &&
+      fieldElements[5].querySelector(crossOrCircle)) ||
+    (fieldElements[6].querySelector(crossOrCircle) !== null &&
+      fieldElements[7].querySelector(crossOrCircle) !== null &&
+      fieldElements[8].querySelector(crossOrCircle)) ||
+    (fieldElements[0].querySelector(crossOrCircle) !== null &&
+      fieldElements[4].querySelector(crossOrCircle) !== null &&
+      fieldElements[8].querySelector(crossOrCircle)) ||
+    (fieldElements[6].querySelector(crossOrCircle) !== null &&
+      fieldElements[4].querySelector(crossOrCircle) !== null &&
+      fieldElements[2].querySelector(crossOrCircle))
+  ) {
+    win = true;
+  }
+};
 
 /* Executed when page is loaded */
 generateField();
